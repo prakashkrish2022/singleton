283c5dc20b5d473f56d532937b8ac52fc668fbd0
prakashkrish2022 <prakashmech925@gmail.com>
2022-04-22 20:04:13 -0700

    singleton code

---
+++ src/com/singleton/Employee.java
@@ -0,0 +1,26 @@
+package com.singleton;
+
+public class Employee {
+	private long maths(long k,long l) {
+	System.out.println(k+l);
+		return 0;
+
+	}
+	public static void main(String[] args) {
+		
+
+		Employee m=new Employee();
+		
+		double maths = m.maths(5, 10);
+		System.out.println(maths);
+		
+		double maths1 = m.maths(15, 10);
+		System.out.println(maths1);
+		
+		double maths2 = m.maths(50, 10);
+		System.out.println(maths2);
+		
+		
+	}
+
+}
